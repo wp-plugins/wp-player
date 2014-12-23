7ab5183949989d29eada3cb0c7520bef9108f51e
jiangyahai <jiangyahai@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 09:25:18 +0000

    V 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-player/trunk@1052370 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/plugin-uploader.js
+++ js/plugin-uploader.js
@@ -1,43 +1,12 @@
-jQuery(document).ready(function() {
-
-	var original_send_to_editor = window.send_to_editor;
-
-	jQuery('.WP-Player-File').click(function() {
-		formField = jQuery(this).prev().attr('id');
-		tb_show('', 'media-upload.php?media-upload.php?type=image&amp;TB_iframe=true');
-		
-		window.send_to_editor = function(html){
-			fileUrl = jQuery( html ).attr('href');
-			jQuery( '#'+formField ).val( fileUrl );
-			tb_remove();
-			window.send_to_editor = original_send_to_editor;
-		}
-		
-		return false;
-		
-	});
-
-	var a = [0,1,2],
-		b = [0,1,3,4,5,6],
-		v = jQuery('input:radio[name="wp_player_type"]:checked').val();
-		
-	wp_player_tabs( v );
-	
-	jQuery('input:radio[name="wp_player_type"]').click(function(){
-		var _eq = jQuery('input:radio[name="wp_player_type"]').index(this);
-		wp_player_tabs( _eq );
-	});
-	
-	function wp_player_tabs( num ){
-		jQuery('#wp-player-tabs tr').hide();
-		if ( num == 0 ){
-			for ( var i=0; i<b.length; i++ ){
-				jQuery('#wp-player-tabs tr').eq(a[i]).show(400);
-			}
-		} else {
-			for ( var i=0; i<b.length; i++ ){
-				jQuery('#wp-player-tabs tr').eq(b[i]).show(400);
-			}
-		}
-	}
-});
+/**
+ * @name     Wp-Player Admin JS
+ * @desc     MetaBox JavaScript
+ * @depend   jQuery
+ * @author   M.J
+ * @date     2014-12-19
+ * @update   2014-12-19
+ * @URL      http://webjyh.com
+ * @version  2.0.0
+ * 
+ */
+jQuery(document).ready(function(){var b=window.send_to_editor;jQuery(".WP-Player-File").on("click",function(){formField=jQuery(this).prev().attr("id");tb_show("","media-upload.php?media-upload.php?type=image&amp;TB_iframe=true");window.send_to_editor=function(a){fileUrl=jQuery(a).attr("href");jQuery("#"+formField).val(fileUrl);tb_remove();window.send_to_editor=b};return false});jQuery("#wp-player-tabs > li").on("click",function(){var a=jQuery(this).index();jQuery(this).addClass("current").siblings().removeClass("current");jQuery("#wp-player-row > div").hide().eq(a).fadeIn()});jQuery("#wp_player_get_xiami_id").on("click",function(){var k=jQuery("#mp3_xiami"),j=jQuery("#mp3_xiami_type"),l=k.val(),i=/^http[s]?:\/\/\w*[\.]?xiami.com+\/(\w+)\/+(\d+).*$/,h={};if(typeof l==="undefined"||l==""){k.focus()}var a=l.match(i);if(jQuery.isArray(a)){h.type=a[1];h.id=a[2]}else{if(!jQuery.isNumeric(l)){alert("获取虾米音乐ID失败！")}}if(jQuery.isArray(a)&&h.type&&h.id){k.val(h.id);j.children("option").prop("selected",false);jQuery("#mp3_xiami_type").find("option[value="+h.type+"]").prop("selected",true)}})});
